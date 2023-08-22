5c6b29208bbc1a4ec90b2a254d6d3cdbbe7074a8
Dahir_Farah <dhrfrx1000@gmail.com>
2023-08-22 13:41:26 +0300

    Initial Creation of the Payment Feature Controller

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace EBS.Controllers
+{
+    public class PaymentController : Controller
+    {
+        // GET: Payment
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Record Payment
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //SET: Record Payment
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        [Display(Name = "Create")]
+        public ActionResult CreateConfirm()
+        {
+            return View();
+        }
+
+
+        //GET: Update Payment
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Display(Name = "Edit")]
+        public ActionResult EditConfirmConfirm()
+        {
+            return View();
+        }
+    }
+}
