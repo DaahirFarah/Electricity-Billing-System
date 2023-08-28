cf2bf42130231ccc3274c15ad7d972ef7e3103ac
Dahir_Farah <dhrfrx1000@gmail.com>
2023-08-28 11:02:26 +0300

    Data Retrieving for Invoices: Done

--- Controllers/InvoiceController.cs
+++ Controllers/InvoiceController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using EBS.viewModels;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,10 +13,15 @@ namespace EBS.Controllers
 {
     public class InvoiceController : Controller
     {
+        // ConnectionString Instance
+        private readonly string SecConn = ConfigurationManager.ConnectionStrings["SecConn"].ConnectionString;
+
+
         // GET: Invoice
         public ActionResult Index()
         {
-            return View();
+            List<invoiceVM> invoices = GetAllInvoices();
+            return View(invoices);
         }
 
         //GET: Register Invoice
@@ -48,5 +56,41 @@ namespace EBS.Controllers
         }
 
         
+        // Fetching Invoices From the Database
+        private List<invoiceVM> GetAllInvoices()
+        {
+            List<invoiceVM> invoices = new List<invoiceVM>();
+
+            using (SqlConnection Connection = new SqlConnection(SecConn))
+            {
+                Connection.Open();
+                string query = "SELECT * FROM InvoiceTbl";
+                using (SqlCommand command = new SqlCommand(query, Connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            invoices.Add(new invoiceVM
+                            {
+                                invoiceID = Convert.ToInt32(reader["invoiceID"]),
+                                cID = Convert.ToInt32(reader["cID"]),
+                                billingPeriodStarts = Convert.ToDateTime(reader["billingPeriodStarts"]),
+                                billingPeriodEnds = Convert.ToDateTime(reader["billingPeriodEnds"]),
+                                prev_Reading = Convert.ToDecimal(reader["prev_Reading"]),
+                                cur_Reading = Convert.ToDecimal(reader["cur_Reading"]),
+                                reading_Date = Convert.ToDateTime(reader["reading_Date"]),
+                                Rate = Convert.ToDecimal(reader["Rate"]),
+                                reading_Value = Convert.ToDecimal(reader["reading_Value"]),
+                                total_Fee = Convert.ToDecimal(reader["total_Fee"]),
+                                
+                            });
+                        }
+                    }
+                }
+            }
+
+            return invoices;
+        }
     }
 }
