28ff1b82f2ba1aeb761a441a3bdda28bac0c20ef
Dahir_Farah <dhrfrx1000@gmail.com>
2023-08-28 11:34:50 +0300

    Insertion Operation Logic For The Invoice Entity is done

--- Controllers/InvoiceController.cs
+++ Controllers/InvoiceController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -35,9 +36,14 @@ namespace EBS.Controllers
         [ValidateAntiForgeryToken]
         [HttpPost]
         [Display(Name = "Create")]
-        public ActionResult CreateConfirm()
+        public ActionResult Create(invoiceVM model)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                InsertInvoice(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
         }
 
 
@@ -91,6 +97,34 @@ namespace EBS.Controllers
             }
 
             return invoices;
+        }
+
+        // Inserting Invoices Into the Database
+        private void InsertInvoice(invoiceVM model)
+        {
+            using(SqlConnection connection = new SqlConnection(SecConn))
+            {
+                connection.Open();
+                string query = "INSERT INTO InvoiceTbl (cID, Rate, billingPeriodStarts,"
+                             + "billingPeriodEnds, prev_Reading, cur_Reading, reading_Value, reading_Date, total_Fee) "
+                             + "VALUES (@cID, @Rate, @billingPeriodStarts, @billingPeriodEnds, @prev_Reading, @cur_Reading,"
+                             + "@reading_Value, @reading_Date, @total_Fee)";
+
+                using(SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@cID", model.cID);
+                    command.Parameters.AddWithValue("@Rate", model.Rate);
+                    command.Parameters.AddWithValue("@billingPeriodStarts", model.billingPeriodStarts);
+                    command.Parameters.AddWithValue("@billingPeriodEnds", model.billingPeriodEnds);
+                    command.Parameters.AddWithValue("@prev_Reading", model.prev_Reading);
+                    command.Parameters.AddWithValue("@cur_Reading", model.cur_Reading);
+                    command.Parameters.AddWithValue("@reading_Value", model.reading_Value);
+                    command.Parameters.AddWithValue("@reading_Date", model.reading_Date);
+                    command.Parameters.AddWithValue("@total_Fee", model.total_Fee);
+
+                    command.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
