043236b9f978e3a618e11829160ae7ab64172b0b
Dahir_Farah <dhrfrx1000@gmail.com>
2023-08-30 09:46:06 +0300

    Data retrieving logic for payments implemented

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using EBS.viewModels;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,10 +14,14 @@ namespace EBS.Controllers
 {
     public class PaymentController : Controller
     {
+        // ConnectionString Instance
+        private readonly string SecConn = ConfigurationManager.ConnectionStrings["SecConn"].ConnectionString;
+
         // GET: Payment
         public ActionResult Index()
         {
-            return View();
+            List<payVM> payments = GetAllPayments();
+            return View(payments);
         }
 
         //GET: Record Payment
@@ -33,18 +41,54 @@ namespace EBS.Controllers
         }
 
 
-        //GET: Update Payment
-        public ActionResult Edit()
+        ////GET: Update Payment
+        //public ActionResult Edit()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //[Display(Name = "Edit")]
+        //public ActionResult EditConfirmConfirm()
+        //{
+        //    return View();
+        //}
+
+        public List<payVM> GetAllPayments()
         {
-            return View();
+            List<payVM> payments = new List<payVM>();
+
+            using (SqlConnection connection = new SqlConnection(SecConn))
+            {
+                connection.Open();
+                string query = "SELECT * FROM PaymentTbl";
+
+                using(SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using(SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            payments.Add(new payVM()
+                            {
+                                payID = Convert.ToInt32(reader["payID"]),
+                                cID = Convert.ToInt32(reader["cID"]),
+                                invoiceID = Convert.ToInt32(reader["invoiceID"]),
+                                paidAmount = Convert.ToDecimal(reader["paidAmount"]),
+                                totalFee = Convert.ToDecimal(reader["totalFee"]),
+                                payMethod = Convert.ToString(reader["payMethod"]),
+                                payDate = Convert.ToDateTime(reader["payDate"]),
+
+                            });
+                        }
+                    }
+                }
+            }
+
+
+            return payments;
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Display(Name = "Edit")]
-        public ActionResult EditConfirmConfirm()
-        {
-            return View();
-        }
     }
 }
