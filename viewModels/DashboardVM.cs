c57c2eeea59204eae118ed7e909457f28c7e2045
Dahir_Farah <dhrfrx1000@gmail.com>
2023-09-06 09:26:46 +0300

    Dash ViewModel added

---
+++ viewModels/DashboardVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EBS.viewModels
+{
+    public class DashboardVM
+    {
+        // I am using this view model to hold the overall statistics data retrieved from the db and then am gonna use it to display that data in the Dashboard page
+
+        public int Customers { get; set; }
+        public decimal Revenue { get; set; }
+        public int Users { get; set; }
+        public int Payments { get; set; }
+        public int Invoices { get; set; }
+        public decimal Balances { get; set; }
+    }
+}
