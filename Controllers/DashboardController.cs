b37bd8380fa2566b6e778bfe97b24bb723a749b1
Dahir_Farah <dhrfrx1000@gmail.com>
2023-09-06 09:21:16 +0300

    Dashboard page added

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,29 @@
+﻿using EBS.viewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EBS.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            List<customerVM> data = GetData();
+            return View(data);
+        }
+
+
+        private List<customerVM> GetData()
+        {
+            List<customerVM> customers = new List<customerVM>();
+
+
+
+            return customers;
+        }
+    }
+}
