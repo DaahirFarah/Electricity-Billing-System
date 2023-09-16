d422df326e78c0afe0e84003284310eafdfd54be
Dahir_Farah <dhrfrx1000@gmail.com>
2023-09-16 12:19:19 +0300

    Added Meter Logic

---
+++ Controllers/MeterController.cs
@@ -0,0 +1,204 @@
+﻿using EBS.viewModels;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EBS.Controllers
+{
+    public class MeterController : Controller
+    {
+        // Instance of the connection String
+        private readonly string SecConn = ConfigurationManager.ConnectionStrings["SecConn"].ConnectionString;
+
+        // GET: Meter
+        public ActionResult Index()
+        {
+            List<MeterVM> meter = GetMeters();
+            return View(meter);
+        }
+
+        // GET: /Meter/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Meter/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(MeterVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                InsertMeter(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        // GET: /Meter/Edit
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            MeterVM meter = GetMeterByID(id);
+            return View(meter);
+        }
+
+        // POST: /Meter/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(MeterVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                UpdateMeter(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        // GET: /Meter/Delete
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            MeterVM meter = GetMeterByID(id);
+            return View(meter);
+        }
+
+        // POST: /Meter/Delete
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DeleteMeter(id);
+            return RedirectToAction("Index");
+        }
+
+
+        // Fetching Meter Information From the Database
+        private List<MeterVM> GetMeters()
+        {
+            List<MeterVM> meters = new List<MeterVM>();
+
+            using (SqlConnection Connection = new SqlConnection(SecConn))
+            {
+                Connection.Open();
+                string query = "SELECT * FROM Meters";
+                using (SqlCommand command = new SqlCommand(query, Connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            meters.Add(new MeterVM
+                            {
+                                MeterID = Convert.ToInt32(reader["MeterID"]),
+                                SerialNumber = Convert.ToInt32(reader["SerialNumber"]),
+                                Type = reader["Type"].ToString(),
+                                InstallationDate = Convert.ToDateTime(reader["InstallationDate"]),
+                                Status = reader["Status"].ToString()
+
+                            });
+                        }
+                    }
+                }
+            }
+
+            return meters;
+        }
+
+        //Get Meter By ID
+        private MeterVM GetMeterByID(int Id)
+        {
+            using (SqlConnection connection = new SqlConnection(SecConn))
+            {
+                connection.Open();
+                string query = "SELECT * FROM Meters WHERE MeterID = @MeterID";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", Id);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            return new MeterVM
+                            {
+                                MeterID = Convert.ToInt32(reader["MeterID"]),
+                                SerialNumber = Convert.ToInt32(reader["SerialNumber"]),
+                                Type = reader["Type"].ToString(),
+                                InstallationDate = Convert.ToDateTime(reader["InstallationDate"]),
+                                Status = reader["Status"].ToString()
+                            };
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        // Insert Meter Data
+        private void InsertMeter(MeterVM model)
+        {
+            using (SqlConnection connection = new SqlConnection(SecConn))
+            {
+                connection.Open();
+                string Insertquery = "INSERT INTO Meters VALUES(@SerialNumber, @Type, @InstallationDate, @Status)";
+
+                using (SqlCommand command = new SqlCommand(Insertquery, connection))
+                {
+                    command.Parameters.AddWithValue("@SerialNumber", model.SerialNumber);
+                    command.Parameters.AddWithValue("@Type", model.Type);
+                    command.Parameters.AddWithValue("@InstallationDate", model.InstallationDate);
+                    command.Parameters.AddWithValue("@Status", model.Status);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Update Meter Info
+        private void UpdateMeter(MeterVM model)
+        {
+            using (SqlConnection connection = new SqlConnection(SecConn))
+            {
+                connection.Open();
+                string UpdateQuery = "UPDATE Meters SET SerialNumber = @SerialNumber, Type = @Type, InstallationDate = @InstallationDate"
+                                   + "Status = @Status";
+
+                using (SqlCommand command = new SqlCommand(UpdateQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@SerialNumber", model.SerialNumber);
+                    command.Parameters.AddWithValue("@Type", model.Type);
+                    command.Parameters.AddWithValue("@InstallationDate", model.InstallationDate);
+                    command.Parameters.AddWithValue("@Status", model.Status);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Delete Meter
+        private void DeleteMeter(int MeterId)
+        {
+            using (SqlConnection connection = new SqlConnection(SecConn))
+            {
+                connection.Open();
+                string DeleteQuery = "DELETE FROM Meter WHERE MeterID = @MeterID";
+
+                using (SqlCommand command = new SqlCommand(DeleteQuery, connection))
+                {
+                    command.Parameters.AddWithValue("MeterID", MeterId);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+    }
+}
